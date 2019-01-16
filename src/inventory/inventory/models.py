9a07a13ce19246683cfb208d29cf683f2b7c4e7e
Georgy Schukin <georgy.schukin@gmail.com>
2019-01-17 00:48:02 +0700

    Start adding models

--- src/inventory/inventory/models.py
+++ src/inventory/inventory/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class InventoryItem(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
